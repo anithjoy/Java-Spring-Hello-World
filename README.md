c03f8eb882ea6269c3c54086c0546e88a1aa49a3
anithjoy <anithjoy1@gmail.com>
2025-07-22 17:42:45 -0400

    second stage of learning

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# Local Repo - Java > DevTrio > QuickStart
+
 # Java-Spring-Hello-World
+
 I am going to implement hello world in spring boot framework
+
+# Learning basics of Spring boot
+
+Here I am going to implement basic workflow of Java SpringBoot
---
+++ Second Stage/product/product/src/main/java/com/example/product/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello from this side";
+    }
+}
